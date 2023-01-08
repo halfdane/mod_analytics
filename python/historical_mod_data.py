df6b5444b1712bc449a330ebf1420551b4bc64c1
halfdane <halfdane@gmx.net>
2023-01-08 15:40:35 +0100

    add configuration

--- python/historical_mod_data.py
+++ python/historical_mod_data.py
@@ -1,1 +1,34 @@
-print('Henlo')
+import asyncio
+import logging
+import re
+import sqlite3
+from datetime import date, datetime, timedelta
+from pprint import pprint
+from unittest.mock import MagicMock
+from urllib.parse import urlparse
+
+import asyncpraw
+
+configuration = ModerationBotConfiguration()
+
+asyncreddit = asyncpraw.Reddit(
+    **configuration.qvbot_reddit_settings(),
+    user_agent="com.halfdane.superstonk_moderation_bot:v0.xx (by u/half_dane)")
+
+async def main():
+    async with asyncreddit as reddit:
+        redditor = await reddit.user.me()
+        print(f"Logged in as {redditor.name}")
+
+        for i in ['DRSyourGME']:
+            await handle_subreddit(await reddit.subreddit(i))
+
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(name)s]: %(message)s'
+)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
